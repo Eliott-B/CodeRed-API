37df0f7f8679211d230c2d60e48251ff9db833bf
Eliott-B <eliottb.info@gmail.com>
2024-09-16 19:22:07 +0200

    :children_crossing: | Add Swagger API doc

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import Database from './modules/database.js';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 import GroupModel from './models/GroupModel.js';
-import EnigmaModel from './models/EnigmaModel.js';
-import SolutionModel from './models/SolutionModel.js';
+// import EnigmaModel from './models/EnigmaModel.js';
+// import SolutionModel from './models/SolutionModel.js';
 
 import groupsRouter from './routes/GroupsRoute.js';
-import enigmasRouter from './routes/EnigmasRoute.js';
-import solutionsRouter from './routes/SolutionsRoute.js';
+// import enigmasRouter from './routes/EnigmasRoute.js';
+// import solutionsRouter from './routes/SolutionsRoute.js';
 
 dotenv.config();
 
@@ -24,19 +26,44 @@ app.use(bodyParser.json());
 const db = new Database();
 
 const groupModel = GroupModel(db.db);
-const enigmaModel = EnigmaModel(db.db);
-const solutionModel = SolutionModel(db.db);
+// const enigmaModel = EnigmaModel(db.db);
+// const solutionModel = SolutionModel(db.db);
 
 await db.connect();
 await db.sync();
 await db.close();
 
 app.use('/api/groups', groupsRouter);
-app.use('/api/enigmas', enigmasRouter);
-app.use('/api/solutions', solutionsRouter);
+// app.use('/api/enigmas', enigmasRouter);
+// app.use('/api/solutions', solutionsRouter);
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "CodeRed API with Swagger",
+      version: "1.0.0",
+      description:
+        "CodeRed API with Swagger",
+      contact: {
+        name: "Eliott B",
+        url: "https://me.eliott-b.fr",
+        email: "eliottb.info@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"],
+};
+const swaggerSpec = swaggerJsdoc(options)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-export { db, groupModel, enigmaModel, solutionModel };
+export { db, groupModel };
--- src/routes/GroupsRoute.js
+++ src/routes/GroupsRoute.js
@@ -6,12 +6,246 @@ import { getAllGroups, getGroupById, createGroup, loginGroup, authGroup, updateG
 
 const groupRouter = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *  name: Groups
+ *  description: Gestion des groupes
+ */
+
+/**
+ * @swagger
+ * components:
+ *  securitySchemes: 
+ *   clientAuth: 
+ *    type: apiKey
+ *    in: header
+ *    name: token
+ *   userAuth:
+ *    type: apiKey
+ *    in: header
+ *    name: groupUUID
+ *   adminAuth:
+ *    type: apiKey
+ *    in: header
+ *    name: admin
+ */
+
+/**
+ * @swagger
+ * /api/groups:
+ *  get:
+ *   tags: [Groups]
+ *   summary: Récupère la liste des groupes
+ *   security:
+ *    - clientAuth: []
+ *    - userAuth: []
+ *   responses:
+ *    200:
+ *     description: Retourne la liste des groupes
+ *    401:
+ *     description: Non autorisé
+ *    404:
+ *     description: Aucun groupe trouvé
+ *    500:
+ *     description: Erreur serveur
+ */
 groupRouter.get('/', authClient, authUser, getAllGroups);
+
+/**
+ * @swagger
+ * /api/groups:id:
+ *  get:
+ *   tags: [Groups]
+ *   summary: Récupère un groupe par son UUID
+ *   security:
+ *    - clientAuth: []
+ *    - userAuth: []
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: string
+ *       format: uuid
+ *      required: true
+ *      description: UUID du groupe
+ *   responses:
+ *    200:
+ *     description: Retourne le groupe
+ *    401:
+ *     description: Non autorisé
+ *    404:
+ *     description: Aucun groupe trouvé
+ *    500:
+ *     description: Erreur serveur
+ */
 groupRouter.get('/:id', authClient, authUser, getGroupById);
+
+/**
+ * @swagger
+ * /api/groups:
+ *  post:
+ *   tags: [Groups]
+ *   summary: Crée un groupe
+ *   security:
+ *    - clientAuth: []
+ *    - userAuth: []
+ *    - adminAuth: []
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       type: object
+ *       properties:
+ *        name:
+ *         type: string
+ *        password:
+ *         type: string
+ *        admin:
+ *         type: boolean
+ *   responses:
+ *    201:
+ *     description: Groupe créé
+ *    401:
+ *     description: Non autorisé
+ *    500:
+ *     description: Erreur serveur
+ */
 groupRouter.post('/', authClient, authUser, isAdmin, createGroup);
+
+/**
+ * @swagger
+ * /api/groups/login:
+ *  post:
+ *   tags: [Groups]
+ *   sumary: Affectue la connexion d'un utilisateur
+ *   security:
+ *    - clientAuth: []
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       type: object
+ *       properties:
+ *        name:
+ *         type: string
+ *        password:
+ *         type: string
+ *   responses:
+ *    200:
+ *     description: Groupe connecté
+ *    401:
+ *     description: Non autorisé
+ *    404:
+ *     description: Groupe non retrouvé
+ *    406:
+ *     description: Mauvais mot de passe
+ *    500:
+ *     description: Erreur serveur
+ */
 groupRouter.post('/login', authClient, loginGroup);
+
+
+/**
+ * @swagger
+ * /api/groups/auth:
+ *  post:
+ *   tags: [Groups]
+ *   sumary: Authentifie un groupe
+ *   security:
+ *    - clientAuth: []
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       type: object
+ *       properties:
+ *        token:
+ *         type: string
+ *   responses:
+ *    200:
+ *     description: Groupe connecté
+ *    401:
+ *     description: Non autorisé
+ *    500:
+ *     description: Erreur serveur
+ */
 groupRouter.post('/auth', authClient, authGroup);
+
+/**
+ * @swagger
+ * /api/groups:id:
+ *  put:
+ *   tags: [Groups]
+ *   summary: Modifie un groupe par son UUID
+ *   security:
+ *    - clientAuth: []
+ *    - userAuth: []
+ *    - adminAuth: []
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: string
+ *       format: uuid
+ *      required: true
+ *      description: UUID du groupe
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       type: object
+ *       properties:
+ *        name:
+ *         type: string
+ *        password:
+ *         type: string
+ *        admin:
+ *         type: boolean
+ *   responses:
+ *    200:
+ *     description: Groupe modifié
+ *    401:
+ *     description: Non autorisé
+ *    404:
+ *     description: Groupe non retrouvé
+ *    500:
+ *     description: Erreur serveur
+ */
 groupRouter.put('/:id', authClient, authUser, updateGroup);
+
+/**
+ * @swagger
+ * /api/groups:id:
+ *  delete:
+ *   tags: [Groups]
+ *   summary: Supprime un groupe par son UUID
+ *   security:
+ *    - clientAuth: []
+ *    - userAuth: []
+ *    - adminAuth: []
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: string
+ *       format: uuid
+ *      required: true
+ *      description: UUID du groupe
+ *   responses:
+ *    200:
+ *     description: Groupe supprimé
+ *    401:
+ *     description: Non autorisé
+ *    404:
+ *     description: Groupe non retrouvé
+ *    500:
+ *     description: Erreur serveur
+ */
 groupRouter.delete('/:id', authClient, authUser, isAdmin, deleteGroup);
 
 export default groupRouter;
